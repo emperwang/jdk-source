b1bfd69c6ae2f58d19c7f2501646a8bf11b3985f
emperwang <544094478@qq.com>
2020-06-10 17:11:14 +0800

    jdk source base; for learn

---
+++ src/wk/Demo1/ArrayListDemo.java
@@ -0,0 +1,9 @@
+package wk.Demo1;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+    public static void main(String[] args) {
+        ArrayList<String> strings = new ArrayList<>();
+    }
+}
